9163ca2b27d10954074b3ddefc287b60f643120e
Jafaros <57791098+Jafaros@users.noreply.github.com>
2021-06-03 07:14:45 +0200

    0.1

---
+++ main.c
@@ -0,0 +1,164 @@
+//Petr Grajciar, 2.B, PVA Bojová aréna, 2.6.2021
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+float winrate(float vyhra, int pocetHer){
+    
+    float winrate;
+
+    winrate = (vyhra / pocetHer) * 100;
+
+    return winrate;
+}
+
+int arena(char name[], int hp, int damage){
+
+    char oponenti[][15] = {"Azog", "SAMS", "Destroyer", "HopperW12", "Zugabuk", "Yasuo", "StileSam", "IN5I", "Risanek1717", "basicallyOndrej"};
+    int index, zapas;
+    bool probihajici_hra = true;
+    
+    printf("===== Zacatek boje =====\n");
+
+    index = rand() % 10;
+    char* oponent = oponenti[index];
+    printf("V arene se utka hrac %s proti %s\n", name, oponent);
+
+    char* bojovnici[2] = {name, oponent};
+
+    int zacina;
+    zacina = rand() % 2;
+
+    printf("Zacina hrac %s\n", bojovnici[zacina]);
+
+    while(probihajici_hra){
+
+        printf("Zivoty hrace %s: %d HP\n", name, hp);
+        printf("Zivoty hrace %s: %d HP\n", oponenti[index], (hp - rand() % 10));
+
+
+        probihajici_hra = false;
+    }
+
+    if(zapas == 1){
+
+        return zapas;
+    }
+
+    return 0;
+}
+
+int pocetHer(int hry){
+
+   printf("Pocet her: %d\n", hry);
+
+   return 0;
+}
+
+int main(void){
+
+    char prikaz[10];
+    int hry = 0, vyhry = 0;
+    float wr;
+
+    int xp = 0;
+    int level = 0;
+
+    //Class v jazyce C
+    struct Bojovnik{
+        char name[20];
+        int hp;
+        int damage;
+    };
+
+    struct Bojovnik fighter;
+
+    srand(time(NULL));
+
+    printf("========== Bojova arena ==========\n");
+
+    //Zadání přezdívky a základní údaje o postavě
+    printf("Zadejte vasi prezdivku: ");
+    scanf("%s", &fighter.name);
+    fighter.hp = 100 * (1 + (level * 0.25));
+    fighter.damage = (rand() % 10) + 15;
+
+    while(1){
+
+        printf("Zadej prikaz, popripade pouzij prikaz |help|: ");
+        scanf("%s", &prikaz);
+
+        //Příkaz, který ukončí program
+        if(strcmp(prikaz, "exit") == 0){
+
+            printf("Ukoncuji program...\n");
+            exit(EXIT_SUCCESS);
+        }
+        //Příkaz, který vám vypíše všechny možné příkazy
+        else if(strcmp(prikaz, "help") == 0){
+
+            printf("=== SEZNAM PRIKAZU ===\n");
+            printf("- arena - Vstoupite do areny\n");
+            printf("- winrate - Zobrazi vam vas pomer vyher a proher\n");
+            printf("- info - Zobrazi vam aktualni status vasi postavy\n");
+            printf("- exit - Vypnuti programu\n");
+            printf("======================\n");
+        }
+        //Příkaz, který vám vypíše aktuální status vaší postavy
+        else if(strcmp(prikaz, "info") == 0){
+            printf("\nVase prezdivka: %s", fighter.name);
+            printf("\nVas rank: ");
+
+            switch(level){
+                case 1:
+                    printf("Zelezna");
+                    break;
+                case 2:
+                    printf("Bronzova");
+                    break;
+                case 3:
+                    printf("Stribrna");
+                    break;
+                case 4:
+                    printf("Zlata");
+                    break;
+                case 5:
+                    printf("Platinova");
+                    break;
+                case 6:
+                    printf("Diamantova");
+                    break;
+                case 7:
+                    printf("Legendarni");
+                    break;
+                default:
+                    printf("Nehodnocena");
+            }
+
+            printf("\nPocet HP: %d\n", fighter.hp);
+            printf("Vas damage: %d\n\n", fighter.damage);
+        }
+        //Zjištění poměru výher a proher
+        else if(strcmp(prikaz, "winrate") == 0){
+
+            wr = winrate(vyhry, hry);
+            printf("Winrate: %.2f %%, Pocet her: %d, Vyhry: %d, Prohry: %d\n", wr, pocetHer(hry), vyhry, (hry - vyhry));
+        }
+        //Vstup do arény
+        else if(strcmp(prikaz, "arena") == 0){
+
+            vyhry += arena(fighter.name, fighter.hp, fighter.damage);
+            hry++;
+        }
+
+        else{
+
+            printf("Neznamy prikaz!\n");
+        }
+    }
+
+    return 0;
+}
